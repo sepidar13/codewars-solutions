ad40ae134a1989723828bf91c4d214cfc6ffe4ce
sepidar13 <f.rabbani1996@gmail.com>
2025-12-04 07:59:59 +0330

    added my second codewar solution to github

---
+++ 8kyu/push an object to an array.js
@@ -0,0 +1,10 @@
+// ** D E S C R I P T I O N **//
+// You are trying to put a hash in ruby or 
+// an object in javascript or java into an array, 
+// but it always returns error, 
+// solve it and keep it as simple as possible!
+
+
+// ** S O L U T I O N **//
+items = []
+items.push({a: "b", c: "d"})
