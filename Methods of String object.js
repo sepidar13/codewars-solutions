4effd8179e1d8752183144dd0c80fd334cce8eba
sepidar13 <f.rabbani1996@gmail.com>
2025-12-09 08:29:16 +0330

    added a new codewar solution

---
+++ Methods of String object.js
@@ -0,0 +1,19 @@
+// *** D E S C R I P T I O N ***//
+
+// Task
+// Coding in function cutIt, function accept 1 parameter:arr. arr is a string array.
+
+// The first mission: Traversing arr, find the shortest string length.
+
+// The second mission: Traversing arr again, intercept all strings to the shortest string length(Start from index0).
+//  you can use one of slice() substring() or substr() do it. return the result after finished the work.
+
+///***  S O L U T I O N ***//
+
+function cutIt(arr){
+  let arrLengths = []
+  let shortestString =  Math.min(...arr.forEach(w => arrLengths.push(w.length)))
+ console.log(arr.map(w => w.substring(0,shortestString)))
+}
+
+cutIt(['dv', 'dcged', 'hodoidh', 'bdws'])
