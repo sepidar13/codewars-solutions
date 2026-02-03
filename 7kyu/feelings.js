e7f4142dcaa70701961115a1745c48d564ecfb90
sepidar13 <134430476+sepidar13@users.noreply.github.com>
2026-02-03 10:47:35 +0330

    Create feelings.js
    added a new code war solution

---
+++ 7kyu/feelings.js
@@ -0,0 +1,40 @@
+Description:
+You have two arguments: string - a string of random letters(only lowercase) and array - an array of strings(feelings). Your task is to return how many specific feelings are in the array.
+
+For example:
+
+string -> 'yliausoenvjw'
+array -> ['anger', 'awe', 'joy', 'love', 'grief']
+output -> '3 feelings.' // 'awe', 'joy', 'love'
+
+
+string -> 'griefgriefgrief'
+array -> ['anger', 'awe', 'joy', 'love', 'grief']
+output -> '1 feeling.' // 'grief'
+
+
+string -> 'abcdkasdfvkadf'
+array -> ['desire', 'joy', 'shame', 'longing', 'fear']
+output -> '0 feelings.'
+If the feeling can be formed once - plus one to the answer.
+
+If the feeling can be formed several times from different letters - plus one to the answer.
+
+Eeach letter in string participates in the formation of all feelings. 'angerw' -> 2 feelings: 'anger' and 'awe'.
+
+
+//Solution :
+function countFeelings(string, array) {
+  let count = 0;
+  
+  array.forEach((item) => {
+      if (item.split('').every((i) => {
+          return string.includes(i); 
+      })) {
+        count++;
+      }
+  });
+  
+  
+  return count == 1 ? `${count} feeling.` : `${count} feelings.`; 
+}
